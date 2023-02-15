3b8332074a95ff01c26746e44ddc8ec58651340b
AndMJ <andrejoao070@gmail.com>
2023-02-15 18:00:26 +0000

    first commit

---
+++ src/main/java/com/api/fh4carlist/Fh4CarListApplication.java
@@ -0,0 +1,13 @@
+package com.api.fh4carlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fh4CarListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fh4CarListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/fh4carlist/Fh4CarListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.fh4carlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fh4CarListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
