1dd287091d70ed39189621104a6ef61fa96c2431
AndMJ <andrejoao070@gmail.com>
2023-02-15 20:30:58 +0000

    ADD car service

---
+++ src/main/java/com/api/fh4carlist/Services/carService.java
@@ -0,0 +1,12 @@
+package com.api.fh4carlist.Services;
+
+import com.api.fh4carlist.Repositorys.carRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class carService {
+
+    @Autowired
+    private carRepository carRepo;
+
+
+}
