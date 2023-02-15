999494f4a61605f39ce2cdfc3d571dad512121ee
AndMJ <andrejoao070@gmail.com>
2023-02-15 18:24:26 +0000

    ADD car model

---
+++ src/main/java/com/api/fh4carlist/Models/car.java
@@ -0,0 +1,32 @@
+package com.api.fh4carlist.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+public class car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID ID;
+
+    @Column(nullable = false)
+    private int carID_inGame;
+
+    @Column(nullable = false)
+    private String carManufacturer;
+
+    @Column(nullable = false)
+    private String carName;
+
+    @Column(nullable = false)
+    private int carYear;
+
+    @Column(nullable = false)
+    private String carType;
+
+    @Column(nullable = true)
+    private String carImage;
+}
