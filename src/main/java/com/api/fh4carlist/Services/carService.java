fa5d055a82accea050da591cb6af18328574fafc
AndMJ <andrejoao070@gmail.com>
2023-02-27 21:56:15 +0000

    ADD add(), findById() & deleteById()

--- src/main/java/com/api/fh4carlist/Services/carService.java
+++ src/main/java/com/api/fh4carlist/Services/carService.java
@@ -1,12 +1,28 @@
 package com.api.fh4carlist.Services;
 
+import com.api.fh4carlist.Models.carModel;
 import com.api.fh4carlist.Repositorys.carRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class carService {
 
     @Autowired
-    private carRepository carRepo;
+    public carRepository carRepo;
 
+    @Transactional
+    public carModel add(carModel car) {
+        return carRepo.save(car);
+    }
 
+    public Optional<carModel> findById(UUID id){
+        return carRepo.findById(id);
+    };
+
+    public void deleteById(UUID id){
+        carRepo.deleteById(id);
+    }
 }
