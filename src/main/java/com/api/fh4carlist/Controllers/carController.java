73bd68aa0e456bddb0a9950321d36cf90eff80e4
AndMJ <andrejoao070@gmail.com>
2023-02-27 21:54:47 +0000

    UPDATE add()
    ADD delete()

--- src/main/java/com/api/fh4carlist/Controllers/carController.java
+++ src/main/java/com/api/fh4carlist/Controllers/carController.java
@@ -1,20 +1,48 @@
 package com.api.fh4carlist.Controllers;
 
+import com.api.fh4carlist.DTOs.carDTO;
+import com.api.fh4carlist.Models.carModel;
 import com.api.fh4carlist.Services.carService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.RequestEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/fh4")
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/fh4")
 public class carController {
 
     @Autowired
-    private carService carServ;
+    public carService carServ;
 
     @PostMapping("/create")
-    public RequestEntity<Object> create (){
+    public ResponseEntity<Object> create (@RequestBody @Valid carDTO dto){
 
-        return null;
+        //cria novo model de car
+        var newCar = new carModel();
+
+        //copia o que vem do request body, depois de validado pelo dto, para o model
+        BeanUtils.copyProperties(dto, newCar);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(carServ.add(newCar));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Object> delete (@PathVariable(value = "id") UUID id){
+        Optional<carModel> carToDelete = carServ.findById(id);
+
+        if (!carToDelete.isEmpty()){
+            carServ.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body("deleted");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body("not found");
+        }
+
     }
 }
