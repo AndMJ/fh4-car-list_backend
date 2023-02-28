52211e2bc3e9d2317f16c514da8775abb136e3a9
AndMJ <andrejoao070@gmail.com>
2023-02-28 00:59:57 +0000

    ADD missing annotations & getters setters

--- src/main/java/com/api/fh4carlist/Models/carModel.java
+++ src/main/java/com/api/fh4carlist/Models/carModel.java
@@ -1,32 +1,87 @@
 package com.api.fh4carlist.Models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
+@Entity
+@Table(name = "TB_CAR")
 public class carModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID ID;
 
     @Column(nullable = false)
-    private int carID_inGame;
+    private int inGameID;
 
     @Column(nullable = false)
-    private String carManufacturer;
+    private String manufacturer;
 
     @Column(nullable = false)
-    private String carName;
+    private String name;
 
     @Column(nullable = false)
-    private int carYear;
+    private int year;
 
     @Column(nullable = false)
-    private String carType;
+    private String type;
 
     @Column(nullable = true)
-    private String carImage;
+    private String image;
+
+    public UUID getID() {
+        return ID;
+    }
+
+    public void setID(UUID ID) {
+        this.ID = ID;
+    }
+
+    public int getInGameID() {
+        return inGameID;
+    }
+
+    public void setInGameID(int inGameID) {
+        this.inGameID = inGameID;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
