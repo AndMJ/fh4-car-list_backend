9546682fd35628173747213c476a94c8b8e98ecd
AndMJ <andrejoao070@gmail.com>
2023-03-07 21:27:07 +0000

    UPDATE list function now working
    ADD start update function

--- src/main/java/com/api/fh4carlist/Controllers/carController.java
+++ src/main/java/com/api/fh4carlist/Controllers/carController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -23,6 +24,10 @@ public class carController {
 
     @PostMapping("/create")
     public ResponseEntity<Object> create (@RequestBody @Valid carDTO dto){
+        //ve se ja existe o carro, por inGameId
+        if (carServ.existsByInGameID(dto.getInGameID())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Ja existe na DB.");
+        }
 
         //cria novo model de car
         var newCar = new carModel();
@@ -31,12 +36,24 @@ public class carController {
 
         //newCar.setImage("");
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(carServ.add(newCar));
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(carServ.add(newCar));
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e);
+        }
     }
 
-    @GetMapping("/list")
-    public ResponseEntity<Object> listAllCars () {
-        return null;
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Object> update (@PathVariable(value = "id") UUID id, @RequestBody @Valid carDTO dto){
+        Optional<carModel> carToUpdate = carServ.findById(id);
+        if(carToUpdate.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("not found");
+        }
+
+        var carUpdate = new carModel();
+        BeanUtils.copyProperties(dto, carUpdate);
+
+        return ResponseEntity.status(HttpStatus.OK).body(carServ.update(id, carUpdate));
     }
 
     @DeleteMapping("/delete/{id}")
@@ -50,5 +67,10 @@ public class carController {
         carServ.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).body("deleted");
 
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<Object> listAllCars () {
+        return ResponseEntity.status(HttpStatus.OK).body(carServ.listAll());
     }
 }
