4cb9189257743f3a30ebeb75f187411ac485314e
AndMJ <andrejoao070@gmail.com>
2023-03-07 21:27:46 +0000

    ADD declaration of method existsByInGameID()

--- src/main/java/com/api/fh4carlist/Repositorys/carRepository.java
+++ src/main/java/com/api/fh4carlist/Repositorys/carRepository.java
@@ -4,7 +4,10 @@ import com.api.fh4carlist.Models.carModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface carRepository extends JpaRepository<carModel, UUID> {}
+public interface carRepository extends JpaRepository<carModel, UUID> {
+    public boolean existsByInGameID(int inGameID);
+}
