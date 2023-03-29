717157429e12a8d97249a4badf50c79720eab3b7
AndMJ <andrejoao070@gmail.com>
2023-03-29 21:33:52 +0100

    UPDATE patch now working

--- src/main/java/com/api/fh4carlist/Controllers/carController.java
+++ src/main/java/com/api/fh4carlist/Controllers/carController.java
@@ -1,10 +1,12 @@
 package com.api.fh4carlist.Controllers;
 
 import com.api.fh4carlist.DTOs.carDTO;
+import com.api.fh4carlist.DTOs.noValidatorsCarDTO;
 import com.api.fh4carlist.Models.carModel;
 import com.api.fh4carlist.Services.carService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,9 +28,9 @@ public class carController {
     public ResponseEntity<Object> bulk_add (@RequestBody @Valid List<carDTO> list){
 
         try {
-            for (carDTO car: list) {
+            for (carDTO car : list) {
                 //ve se ja existe o carro, por inGameId
-                if (carServ.existsByInGameID(car.getInGameID())){
+                if (carServ.existsByInGameID(car.getInGameID())) {
                     //return ResponseEntity.status(HttpStatus.CONFLICT).body("Ja existe na DB.");
                     System.out.printf("Ja existe na DB.");
                     continue;
@@ -43,7 +45,7 @@ public class carController {
             }
 
             return ResponseEntity.status(HttpStatus.CREATED).body("All created with success.");
-        } catch (Exception e){
+        } catch (BeansException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e);
         }
     }
@@ -86,26 +88,35 @@ public class carController {
         return ResponseEntity.status(HttpStatus.OK).body(carServ.save(carUpdate));
     }
 
-//    @PatchMapping("/update/{id}")
-//    public ResponseEntity<Object> updatePatch (@PathVariable(value = "id") UUID id, @RequestBody carDTO dto){
-//        Optional<carModel> carToUpdate = carServ.findById(id);
-//        if(carToUpdate.isEmpty()){
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("not found");
-//        }
-//
-//        var carUpdate = carToUpdate.get();
-//
-//        carUpdate.setName(dto.getName());
-//        carUpdate.setManufacturer(dto.getManufacturer());
-//        carUpdate.setYear(dto.getYear());
-//        carUpdate.setType(dto.getType());
-//        carUpdate.setImage(dto.getImage());
-//        carUpdate.setInGameID(dto.getInGameID());
-//
-//        return ResponseEntity.status(HttpStatus.OK).body(carServ.save(carUpdate));
-//        //TODO: PATCH, maybe create a carDTO without the @annotations to validate and use it instead
-//        // or not because its redundancy
-//    }
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<Object> patchUpdate (@PathVariable(value = "id") UUID id, @RequestBody noValidatorsCarDTO dto){
+        Optional<carModel> carToUpdate = carServ.findById(id);
+        if(carToUpdate.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("not found");
+        }
+
+        var carUpdate = carToUpdate.get();
+        if(dto.getName() != null){
+            carUpdate.setName(dto.getName());
+        }
+        if(dto.getManufacturer() != null){
+            carUpdate.setManufacturer(dto.getManufacturer());
+        }
+        if(dto.getYear() != null){
+            carUpdate.setYear(dto.getYear());
+        }
+        if(dto.getType() != null){
+            carUpdate.setType(dto.getType());
+        }
+        if(dto.getImage() != null){
+            carUpdate.setImage(dto.getImage());
+        }
+        if(dto.getInGameID() != null){
+            carUpdate.setInGameID(dto.getInGameID());
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(carServ.save(carUpdate));
+    }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Object> delete (@PathVariable(value = "id") UUID id){
@@ -118,6 +129,13 @@ public class carController {
         carServ.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).body("deleted");
 
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Object> findCar (@PathVariable(value = "id") UUID id) {
+        Optional<carModel> GottenCar = carServ.findById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(GottenCar);
     }
 
     @GetMapping("/list")
