8923e62dd587368d69722ace2620f65996002d7f
AndMJ <andrejoao070@gmail.com>
2023-04-21 01:15:12 +0100

    house cleaning

--- src/main/java/com/api/fh4carlist/Repositorys/user/userRepository.java
+++ src/main/java/com/api/fh4carlist/Repositorys/user/userRepository.java
@@ -2,10 +2,15 @@ package com.api.fh4carlist.Repositorys.user;
 
 import com.api.fh4carlist.Models.user.userModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface userRepository extends JpaRepository<userModel, UUID> {
+    //get just the user data, with no cars owned
+    //@Query(value = "SELECT id,name,email,password FROM userModel WHERE id = ?1")
+    //public Optional<Object> getUserData(UUID id);
 }
--- src/main/java/com/api/fh4carlist/Services/user/userService.java
+++ src/main/java/com/api/fh4carlist/Services/user/userService.java
@@ -1,6 +1,5 @@
 package com.api.fh4carlist.Services.user;
 
-import com.api.fh4carlist.Models.car.carModel;
 import com.api.fh4carlist.Models.user.userModel;
 import com.api.fh4carlist.Repositorys.car.carRepository;
 import com.api.fh4carlist.Repositorys.user.userRepository;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -25,6 +23,13 @@ public class userService {
     public userModel save(userModel user){
         return userRepo.save(user);
     }
+
+    //find user data by id
+    /*public Optional<Object> findUserDataById(UUID id) {
+        Object userdata = userRepo.getUserData(id);
+        userdata.
+        return
+    }*/
 
     //find user by id
     public Optional<userModel> findById(UUID id){
